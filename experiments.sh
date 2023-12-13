363c4dd0ce784a444fbef6441b0ee9934e40737c
MrCogito <karoljedr@gmail.com>
2023-12-13 21:47:14 +0100

    Just Trained

--- experiments.sh
+++ experiments.sh
@@ -3,7 +3,7 @@
 # bsub -o "outputs/Test80/Markdown/Test80_0.md" -J "Test80_0" -env MYARGS="-name Test80-0 -time 84600 -epochs 1000 -batch_size 32 -isServer True -gamma 99.0 -ID 0" < submit_gpu_a80.sh
 # mkdir -p outputs/Test40/Markdown
 # bsub -o "outputs/Test40/Markdown/Test40_0.md" -J "Test40_0" -env MYARGS="-name Test40-0 -time 84600 -epochs 1000 -batch_size 32 -isServer True -gamma 99.0 -ID 0" < submit_gpu_a40.sh
-mkdir -p outputs/20kTest32/Markdown
-bsub -o "outputs/20kTest32/Markdown/20kTest32_0.md" -J "20kTest32_0" -env MYARGS="-name 20kTest32-0 -time 84600 -epochs 10000 -batch_size 32 -isServer True -gamma 99.0 -ID 0" < submit_gpu_v32.sh
-# mkdir -p outputs/Test16/Markdown
-# bsub -o "outputs/Test16/Markdown/Test16_0.md" -J "Test16_0" -env MYARGS="-name Test16-0 -time 84600 -epochs 1000 -batch_size 32 -isServer True -gamma 99.0 -ID 0" < submit_gpu_v16.sh
+mkdir -p outputs/exp20kTest32/Markdown
+bsub -o "outputs/exp20kTest32/Markdown/exp20kTest32_0.md" -J "exp20kTest32_0" -env MYARGS="-name exp20kTest32-0 -time 84600 -epochs 10000 -batch_size 32 -isServer True -gamma 99.0 -ID 0" < submit_gpu_v32.sh
+mkdir -p outputs/Test16/Markdown
+bsub -o "outputs/Test16/Markdown/expTest16_0.md" -J "Test16_0" -env MYARGS="-name Test16-0 -time 84600 -epochs 1000 -batch_size 32 -isServer True -gamma 99.0 -ID 0" < submit_gpu_v16.sh
